6c03573691cbd69a0d452fffb394384fdd5aeee7
JacobPisarcikTU <tut42885@temple.edu>
2026-03-31 14:21:24 -0400

    initial commit

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Price = 16.99M }
+        };
+
+        public ProductsController()
+        {
+
+        }
+
+        public ProductsController(Product[] products)
+        {
+            this.products = products;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductsApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsApp.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
